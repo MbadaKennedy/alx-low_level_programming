a8f68d330ba59dbcfcdd90185f5d2ae8f9ba1656
MbadaKennedy <kennedyomeile@gmail.com>
2023-09-09 14:16:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Returns: 0 if succes.
- */
+ *  * main - Entry point of the program
+ *   *
+ *    * Return: Always 0 (Success)
+ * * Description: This function is the entry point of the program and returns 0
+ **/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
