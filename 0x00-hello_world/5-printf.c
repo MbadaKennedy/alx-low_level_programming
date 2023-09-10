2dbd9604e953cab112d0d84b11e071c8020f74c5
MbadaKennedy <kennedyomeile@gmail.com>
2023-09-10 04:20:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A C program that prints a string on execution
+ *
+ * Description: main - Prints out a string to studio.
+ * Return: 0 if success
+ **/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
