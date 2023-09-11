f75179764be47781bb13927420b857757e03ee29
MbadaKennedy <kennedyomeile@gmail.com>
2023-09-11 15:47:33 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - The beginning of the program
+  *Return: 0
+**/
+
+int main(void)
+
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
+
