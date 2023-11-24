3fe01c51e13eae80ee32e2c10ff928d1e0787424
Kennedy Omeile <kennedyomeile@gmail.com>
2023-11-24 13:24:01 -0800

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -49,6 +49,21 @@ void errors(void)
 	exit(98);
 }
 
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
